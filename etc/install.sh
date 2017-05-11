f11c5c28f1824d84b90f248bf74f685d3ef793a8
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-05-11 01:22:15 +0000

    refs #6 dry-run を実装

--- etc/install.sh
+++ etc/install.sh
@@ -30,6 +30,6 @@ do
 done
 
 # 環境に Vim がインストールされている場合は Vim 関連の install.sh は実行しない
-is_installed vim || find ${DOTVIM}/etc/install/ -type d -name "01*" | sort | xargs -i sh {}/install.sh
+is_installed vim || find ${DOTVIM}/etc/install/ -type d -name "01*" | sort | xargs -i ${dryrun} sh {}/install.sh
 
 exit 0
